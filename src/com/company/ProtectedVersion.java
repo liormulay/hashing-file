1bcc9510e309952f026a580b47d158dca285a485
Lior Mulay <lior.mulay@stargo.co>
2022-07-01 15:22:45 +0300

    Initial commit

---
+++ src/com/company/ProtectedVersion.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.File;
+
+public class ProtectedVersion {
+    private String h0;
+    private File protectedFile;
+
+    public String getH0() {
+        return h0;
+    }
+
+    public void setH0(String h0) {
+        this.h0 = h0;
+    }
+
+    public File getProtectedFile() {
+        return protectedFile;
+    }
+
+    public void setProtectedFile(File protectedFile) {
+        this.protectedFile = protectedFile;
+    }
+}
