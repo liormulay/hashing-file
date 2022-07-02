a8c39ebacf9ec6433d1ac470b5d8fa7455012d99
Lior Mulay <lior.mulay@stargo.co>
2022-07-02 23:17:20 +0300

    create utils test

---
+++ src/test/UtilsTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+class UtilsTest {
+
+    @Test
+    void mergeArrays() {
+    }
+
+    @Test
+    void sha() {
+    }
+
+    @Test
+    void encodeHexString() {
+    }
+
+    @Test
+    void byteToHex() {
+    }
+
+    @Test
+    void readFileToBytes() {
+    }
+
+    @Test
+    void createHashing() {
+    }
+
+    @Test
+    void createFile() {
+    }
+}
